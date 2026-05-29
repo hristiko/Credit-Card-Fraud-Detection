5af07351e62a232e77890b989a40b2abf187abb1
HK04 <hristiko@hotmail.com>
2026-05-29 16:53:39 +0200

    I modified the preprocessing function such that instead of directly working on the training and testing sets I maked copy of them in order to avoid unwanted changes in the original versions. Add a oversapling method that uses random duplication (I did't make it with SMOTE because this technique could potentially create samples that don't represnt real fraud cases)

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -7,13 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils.class_weight import compute_class_weight
 from config import TEST_SIZE, RANDOM_SEED
 
-def loadData(path):
+def load_data(path):
     
     dataset = pd.read_csv(path)
     print(f"Dataset loaded: {dataset.shape[0]:,} rows, {dataset.shape[1]} columns")
     return dataset
 
-def preprocessData(dataset):
+def preprocess_data(dataset):
 
     X = dataset.drop(columns="Class")
     y = dataset["Class"].values
@@ -26,15 +26,15 @@ def preprocessData(dataset):
         stratify=y
     )
 
-    scalar = StandardScaler()
+    scaler = StandardScaler()
 
     X_train = X_train.copy()
     X_test = X_test.copy()
 
     columns_to_scale = ["Time", "Amount"]
 
-    X_train[columns_to_scale] = scalar.fit_transform(X_train[columns_to_scale])
-    X_test[columns_to_scale] = scalar.transform(X_test[columns_to_scale])
+    X_train[columns_to_scale] = scaler.fit_transform(X_train[columns_to_scale])
+    X_test[columns_to_scale] = scaler.transform(X_test[columns_to_scale])
 
     X_train = X_train.values
     X_test = X_test.values
@@ -45,18 +45,19 @@ def preprocessData(dataset):
     fraudPrecentage = distTrain.get(1, 0) / sum(distTrain.values()) * 100
 
     print(
-        f"\n Training set: {X_train.shpae[0]:, } samples"
+        f"\n Training set: {X_train.shape[0]:, } samples"
         f"({distTrain.get(0, 0):,} legit | {distTrain.get(1,0):,} fraud)"
     )
 
     print(
-        f"\n Test set: {X_test.shpae[0]:, } samples"
+        f"\n Test set: {X_test.shape[0]:, } samples"
         f"({distTest.get(0, 0):,} legit | {distTest.get(1,0):,} fraud)"
     )
 
     print(f"Fraud ratio in training: {fraudPrecentage:.4f}% \n")
 
-    return X_train, X_test, y_train, y_test, scalar
+    return X_train, X_test, y_train, y_test, scaler
+    
 
 def save_scalar(scalar, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -65,3 +66,33 @@ def save_scalar(scalar, path):
         pickle.dump(scalar, f)
 
     print(f"Scalar saved in: {path}")
+
+def oversample_minoroty(X_train, y_train, random_state = RANDOM_SEED):
+    fraud_indicies = np.where(y_train == 1)[0]
+    legit_indicies = np.where(y_train == 0)[0]
+
+    n_fraud = len(fraud_indicies)
+    n_legit = len(legit_indicies)
+
+    fraud_repeat_count = n_legit // n_fraud
+
+    print(f"Oversampling: repeating {n_fraud} fraud samples * {fraud_repeat_count}, which is {n_fraud * fraud_repeat_count} fraud samples")
+
+    oversampled_fraud_indicies = np.tile(fraud_indicies, fraud_repeat_count)
+
+    all_indicies = np.concatenate([legit_indicies, oversampled_fraud_indicies])
+
+    local_random_generator = np.random.default_rng(random_state)
+    local_random_generator.shuffle(all_indicies)
+
+    X_balanced = X_train[all_indicies]
+    y_balanced = y_train[all_indicies]
+
+    print(f"Balanced set: {np.sum(y_balanced=0)} legit | np.sum(y_balanced == 1) fraud\n")
+
+    return X_balanced, y_balanced
+
+
+def get_class_distribution(y):
+    unique, counts = np.unique(y, return_counts=True)
+    return dict(zip(unique, counts))
